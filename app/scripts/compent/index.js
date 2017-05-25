f345da65ac784f50b7b719e38b1bf7b5ff76bc7a
wenlisu <415357008@qq.com>
2017-05-25 19:08:56 +0800

    update

--- app/scripts/compent/index.js
+++ app/scripts/compent/index.js
@@ -1,11 +1,11 @@
-
-layui.use('element', function(exports){
+var fetchPost = require("../util");
+console.log("fetchPost",fetchPost);
+   layui.use('element', function(exports){
   var element = layui.element(); //导航的hover效果、二级菜单等功能，需要依赖element模块
   
   //监听导航点击
   element.on('nav(demo)', function(elem){
     layer.msg(elem.text());
   });
-  exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
 
 });
