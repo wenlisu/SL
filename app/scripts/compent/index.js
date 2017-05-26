50ac4840b5285e24f056fd74e63b8a0cc82fdaeb
wenlisu <415357008@qq.com>
2017-05-26 17:51:32 +0800

    添加fetch、config、route

--- app/scripts/compent/index.js
+++ app/scripts/compent/index.js
@@ -1,9 +1,21 @@
-layui.use(['element', 'utilFetch'], function(exports) {
-    var element = layui.element(); //导航的hover效果、二级菜单等功能，需要依赖element模块
-    layer.msg(layui.utilFetch());
+layui.use(['element', '_fetch', '_config', '_route'], function(exports) {
+    var element = layui.element() //导航的hover效果、二级菜单等功能，需要依赖element模块
+    , _route = layui._route
+    ,_fetch = layui._fetch
+     ,api = layui._config.api;
+
+    _fetch(api + 'user/login', {
+        "phone": "15298765432",
+        "password": "123456"
+    }).then(function(data) {
+        // console.log("data", data);
+    }, function(err) {
+        console.log("err", err);
+    });
     //监听导航点击
     element.on('nav(demo)', function(elem) {
-        layer.msg(elem.text());
+        var mUrl = elem.attr('qqm-menu');
+        !_.isEmpty(mUrl) && _route.go(mUrl);
     });
 
 });
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -29,8 +29,13 @@ gulp.task('minifycss', () => {
 });
 gulp.task('minifyjs', function() {
     return gulp.src('app/scripts/**/*.js')
-        .pipe(gulp.dest('dist/scripts'));  
+        .pipe(gulp.dest('dist/scripts'));
 });
+gulp.task('compentHtml', function() {
+    return gulp.src('app/modules/*.html')
+        .pipe(gulp.dest('dist/modules'));
+});
+
 function lint(files, options) {
     return () => {
         return gulp.src(files)
@@ -48,6 +53,8 @@ const testLintOptions = {
 
 gulp.task('lint', lint('app/scripts/**/*.js'));
 gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
+
+
 
 gulp.task('html', ['styles'], () => {
     const assets = $.useref.assets({ searchPath: ['.tmp', 'app', '.'] });
@@ -78,12 +85,10 @@ gulp.task('images', () => {
         .pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('fonts', () => {
-    return gulp.src(require('main-bower-files')({
-            filter: '**/*.{eot,svg,ttf,woff,woff2}'
-        }).concat('app/fonts/**/*'))
-        .pipe(gulp.dest('.tmp/fonts'))
-        .pipe(gulp.dest('dist/fonts'));
+gulp.task('font', () => {
+    return gulp.src('app/font/**/*')
+        .pipe(gulp.dest('.tmp/font'))
+        .pipe(gulp.dest('dist/font'));
 });
 
 gulp.task('extras', () => {
@@ -97,7 +102,7 @@ gulp.task('extras', () => {
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-gulp.task('serve', ['styles', 'fonts'], () => {
+gulp.task('serve', ['styles', 'font'], () => {
     browserSync({
         notify: false,
         port: 9000,
@@ -118,7 +123,7 @@ gulp.task('serve', ['styles', 'fonts'], () => {
 
     gulp.watch('app/styles/**/*.scss', ['styles']);
     gulp.watch('app/fonts/**/*', ['fonts']);
-    gulp.watch('bower.json', ['wiredep', 'fonts']);
+    gulp.watch('bower.json', ['wiredep', 'font']);
 });
 
 gulp.task('serve:dist', () => {
@@ -164,7 +169,7 @@ gulp.task('wiredep', () => {
         .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['html', 'images', 'fonts', 'extras', 'minifycss', 'minifyjs'], () => {
+gulp.task('build', ['html', 'images', 'font', 'extras', 'minifycss', 'minifyjs', 'compentHtml'], () => {
     return gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true }));
 });
 
