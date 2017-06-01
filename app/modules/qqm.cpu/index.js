a5439fd537ee5510e0b75c2ac2e3dc4ad2722d69
wenlisu <415357008@qq.com>
2017-06-01 12:11:53 +0800

    添加运维工具、机房服务器监控

--- app/modules/qqm.cpu/index.js
+++ app/modules/qqm.cpu/index.js
@@ -3,15 +3,15 @@ layui.use(['form', 'laypage', '_config', '_route', '_fetch', '_view'], function(
 'use strict';
     var _route = layui._route,
     form = layui.form(),
-    _view = new layui._view('#logView');
+    _view = new layui._view('#cpuView');
  // 初始化当前位置
-  _route.setBreadcrumb(['后台log日志', '后端log实时日志']);
+  _route.setBreadcrumb(['机房服务器监控', '华为三层交换机192.168.40.246']);
   // 渲染表单
   form.render();
   //监听提交
   form.on('submit(formDemo)', function(data){
     layer.msg(JSON.stringify(data.field));
-    _view.render($("#logView"),data);
+    _view.render($("#cpuView"),data);
     return false;
   });
 
--- app/modules/qqm.internet/index.js
+++ app/modules/qqm.internet/index.js
@@ -3,15 +3,15 @@ layui.use(['form', 'laypage', '_config', '_route', '_fetch', '_view'], function(
 'use strict';
     var _route = layui._route,
     form = layui.form(),
-    _view = new layui._view('#logView');
+    _view = new layui._view('#internetView');
  // 初始化当前位置
-  _route.setBreadcrumb(['后台log日志', '后端log实时日志']);
+  _route.setBreadcrumb(['机房服务器监控', '高志大厦20楼网络连通性']);
   // 渲染表单
   form.render();
   //监听提交
   form.on('submit(formDemo)', function(data){
     layer.msg(JSON.stringify(data.field));
-    _view.render($("#logView"),data);
+    _view.render($("#internetView"),data);
     return false;
   });
 
